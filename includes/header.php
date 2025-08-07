ef87758362d017319326c40ad1fdac6e3ae8972a
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-08-06 22:13:49 -0300

    :bug: Corrigindo bobeira pela n-sima vez

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.4/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="../assets/css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/custom.css">
+
 </head>
 <body id="page-top">
-<!-- Não feche body ou html aqui -->
